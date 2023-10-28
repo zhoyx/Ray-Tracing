abc318c1c47c9ee03aaff742ec28ce43440f3844
zhoyx <zhouyx2000@gmail.com>
2023-10-28 16:01:18 +0800

    initialize utility header file and integrate hittable

--- hittable_list.h
+++ hittable_list.h
@@ -18,13 +18,21 @@ public:
         objects.push_back(object);
     }
 
+    void clear() {
+        objects.clear();
+    }
+
+    void add(shared_ptr<hittable> object) {
+        objects.push_back(object);
+    }
+
     bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
         hit_record temp_rec{};
         bool hit_anything = false;
         auto closest_so_far = ray_tmax;
 
         for (const auto& object : objects) {
-            if (object->hit(r, ray_tmin, ray_tmax, temp_rec)) {
+            if (object->hit(r, ray_tmin, closest_so_far, temp_rec)) {
                 hit_anything = true;
                 closest_so_far = temp_rec.t;
                 rec = temp_rec;
