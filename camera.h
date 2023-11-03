e2ff27a56f8f5f72944bbaec0d9fc7fcb034a6d9
zhoyx <zhouyx2000@gmail.com>
2023-11-03 22:06:39 +0800

    create reflective material

--- camera.h
+++ camera.h
@@ -5,6 +5,7 @@
 
 #include "color.h"
 #include "hittable.h"
+#include "material.h"
 
 #include <iostream>
 
@@ -96,6 +97,14 @@ private:
         }
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
+            ray scattered;
+            color attenuation;
+
+            if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+                return attenuation * ray_color(scattered, depth - 1, world);
+            }
+            return color(0, 0, 0);
+
             vec3 direction = rec.normal + random_unit_vector();
             return 0.1 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
--- sphere.h
+++ sphere.h
@@ -1,12 +1,13 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
+#include "rtweekend.h"
 #include "hittable.h"
-#include "vec3.h"
+
 
 class sphere : public hittable {
 public:
-    sphere(point3 _center, double _radius) : center(_center), radius(_radius) {}
+    sphere(point3 _center, double _radius, shared_ptr<material> _material) : center(_center), radius(_radius), mat(_material) {}
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = r.origin() - center;
@@ -31,6 +32,7 @@ public:
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -39,6 +41,7 @@ public:
 private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
